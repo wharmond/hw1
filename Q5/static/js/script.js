5148488ae8168433e3504a8bd3118991426ecb55
wharmond <43218226+wharmond@users.noreply.github.com>
2018-09-15 10:51:30 -0400

    Add files via upload

--- Q5/static/js/script.js
+++ Q5/static/js/script.js
@@ -2,13 +2,32 @@ $(function(){
 	$('button').click(function(){
 		var user = $('#inputUsername').val();
 		var pass = $('#inputPassword').val();
+        var resp_text = []
 		$.ajax({
 			url: '/signUpUser',
 			data: $('form').serialize(),
 			type: 'POST',
 			success: function(response){
-				console.log(response);
-			},
+                    var obj = JSON.parse(response);
+                    if (obj.status == 'OK') {
+                        $("#valid-pass").removeAttr('hidden')
+                                        .text("Congratulations on registering for CSE6242, " + user + ". Redirecting you to the course homepage...");
+                        $("#inputUsername").val("");
+                        $("#inputPassword").val("");
+                        console.log(response);
+                        setTimeout(openPopup, 3000);
+                    }
+                    else{
+                        $("#inputPassword").val("");
+                        var errorMessage = ''
+                        var errorResponses = ["Should be at least 8 characters in length. ","Should have at least 1 uppercase character. ","Should have at least 1 number. "];
+                        obj.pass.forEach(function(e_m){
+                            errorMessage += errorResponses[e_m];
+                        });
+                        $("#invalid-pass").removeAttr('hidden')
+                                          .text(user + ", the password is invalid: " + errorMessage + "Please try again");
+                        }
+            },
 			error: function(error){
 				console.log(error);
 			}
@@ -16,3 +35,6 @@ $(function(){
 	});
 });
 
+function openPopup(){
+    window.location.href = "http://poloclub.gatech.edu/cse6242/";
+}
